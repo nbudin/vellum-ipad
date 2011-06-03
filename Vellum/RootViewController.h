43fbe5fd21f4880bfc770269aa94c1d23253b17e
Nat Budin <natbudin@gmail.com>
2011-06-03 14:36:19 -0400

    Initial Commit

---
+++ Vellum/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Vellum
+//
+//  Created by Nat Budin on 6/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ Vellum/VellumAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  VellumAppDelegate.h
+//  Vellum
+//
+//  Created by Nat Budin on 6/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VellumAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
